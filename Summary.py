69ac6c3f4afea2a656a184f9a7f1ac13e1ccf6e7
SaraAlkurdy <sara.alkurdy96@yahoo.com>
2018-04-30 18:59:39 +0300

    text summarization

---
+++ Summary.py
@@ -0,0 +1,59 @@
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+
+
+stemmer = SnowballStemmer('english')
+stopWords = set(stopwords.words('english'))
+
+
+file=open('data.txt','rU')
+text=file.read()
+words = word_tokenize(text)
+
+
+freqTable = dict()
+for word in words:
+    word = word.lower()
+    if word in stopWords:
+        continue
+
+    word = stemmer.stem(word)
+    
+    if word in freqTable:
+        freqTable[word] += 1
+    else:
+        freqTable[word] = 1
+
+##print(freqTable)
+
+
+sentences = sent_tokenize(text)
+sentenceValue = dict()
+
+
+for sentence in sentences:
+    for word, freq in freqTable.items():
+        if word in sentence.lower():
+            if sentence in sentenceValue:
+                sentenceValue[sentence] += freq
+            else:
+                sentenceValue[sentence] = freq
+
+
+sumValues = 0
+for sentence in sentenceValue:
+    sumValues += sentenceValue[sentence]
+
+
+# Average value of a sentence from original text
+average = int(sumValues / len(sentenceValue))
+
+
+summary = ""
+for sentence in sentences:
+        if sentence in sentenceValue and sentenceValue[sentence] > (1.3 * average):
+            summary +=  " " + sentence
+            
+print(summary)
+
